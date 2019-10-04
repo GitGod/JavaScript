d63214a96b6a2dcad7107ba0830b20de68ffc46e
GitGod <hirakei89@gmail.com>
2019-10-04 18:39:56 +0200

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,40 @@
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {counter: 0};
+  }
+  
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment10.bind(this)}>+10</button>
+        <button onClick={this.increment.bind(this)}>+</button>
+        <output>{this.state.counter}</output>
+        <button onClick={this.decrement.bind(this)}>-</button>         <button onClick={this.decrement10.bind(this)}>-10</button>
+      </div>
+    );
+  }
+  
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+  increment10() {
+    this.setState({
+      counter: this.state.counter + 10
+    })
+  }
+  
+  decrement() {
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+   decrement10() {
+    this.setState({
+      counter: this.state.counter - 10
+    })
+  }
+}
+ReactDOM.render(<App />, document.getElementById("app"));

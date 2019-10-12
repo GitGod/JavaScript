c01dfc30c9cbbdab28d807349b3baf88c976357c
GitGod <hirakei89@gmail.com>
2019-10-12 19:04:20 +0200

    Update ClassExampleReact.js

--- ClassExampleReact.js
+++ ClassExampleReact.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const fiftyFifty = Math.random() < 0.5;
+const fiftypercent = Math.random() < 0.5;
 
 class Randomer extends React.Component {
   render() {
-    if (fiftyFifty) {
+    if (fiftypercent) {
       return <h1>O</h1>;
     } else {
       return <h1>X</h1>;
